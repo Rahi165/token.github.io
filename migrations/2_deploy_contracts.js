bfd0c5176a72e3e9c0ecf780301b2c290a6b7d06
Rahi165 <97022750+Rahi165@users.noreply.github.com>
2023-03-26 22:09:58 +0530

    Compiles assets for Github Pages

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var ShailToken = artifacts.require("./ShailToken.sol");
+var ShailTokenSale = artifacts.require("./ShailTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ShailToken);
+  deployer.deploy(ShailToken, 1000000).then(function(){
+    var tokenPrice = 1000000000000000;
+
+  return deployer.deploy(ShailTokenSale, ShailToken.address, tokenPrice);
+});
 };
